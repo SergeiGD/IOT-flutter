8219c88eafdda08aaa196cee785abfa7c01cee9c
Sergei Golovin <sergeigolovin.dm@gmail.com>
2023-10-29 13:47:13 +0800

    update nav

--- iot/lib/LoginScreen/login_screen.dart
+++ iot/lib/LoginScreen/login_screen.dart
@@ -1,2 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:iot/General/components/bottom_navigation.dart';
+import 'package:iot/constants.dart';
 
-// todo
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Container(
+        width: size.width,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            stops: [0.3, 0.5],
+            colors: [
+              cMainBackgroundColor,
+              cSecondaryBackgroundColor,
+            ],
+          ),
+        ),
+        child: Container(),
+      ),
+      bottomNavigationBar: const BottonNavigation(
+        currentPage: Pages.profile,
+      ),
+    );
+  }
+}
