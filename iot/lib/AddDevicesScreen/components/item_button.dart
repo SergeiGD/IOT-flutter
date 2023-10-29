702ea6af3247353151e9e9393c5b862aca509f86
Sergei Golovin <sergeigolovin.dm@gmail.com>
2023-10-29 14:11:48 +0800

    edit devices items

--- iot/lib/AddDevicesScreen/components/item_button.dart
+++ iot/lib/AddDevicesScreen/components/item_button.dart
@@ -1,62 +1,36 @@
 import 'package:flutter/material.dart';
-import 'package:iot/constants.dart';
 
-class ItemButton extends StatefulWidget {
-  const ItemButton({
-    super.key,
-    required this.size,
-    required this.icon,
-    required this.title,
-  });
+class ItemButton extends StatelessWidget {
+  const ItemButton(
+      {super.key,
+      required this.size,
+      required this.image,
+      required this.title});
 
   final Size size;
-  final IconData icon;
+  final Image image;
   final String title;
-
-  @override
-  State<ItemButton> createState() => _ItemButtonState();
-}
-
-class _ItemButtonState extends State<ItemButton> {
-  bool isSelected = false;
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         GestureDetector(
-          onTap: () {
-            setState(() {
-              isSelected = !isSelected;
-            });
-          },
-          child: Container(
-            height: widget.size.height * 0.105,
-            width: widget.size.width * 0.21,
-            decoration: BoxDecoration(
-              color: isSelected ? cGreenColor : Colors.white,
+          onTap: () => (),
+          child: SizedBox(
+            height: size.height * 0.12,
+            width: size.width * 0.21,
+            child: ClipRRect(
               borderRadius: BorderRadius.circular(20),
-              boxShadow: [
-                BoxShadow(
-                    color: isSelected
-                        ? cGreenColor.withOpacity(0.5)
-                        : Colors.grey.shade200,
-                    blurRadius: 30,
-                    offset: const Offset(5, 5)),
-              ],
-            ),
-            child: Icon(
-              widget.icon,
-              size: 45,
-              color: isSelected ? Colors.white : cSecondaryBackgroundColor,
+              child: image,
             ),
           ),
         ),
         SizedBox(
-          height: widget.size.height * 0.01,
+          height: size.height * 0.01,
         ),
         Text(
-          widget.title,
+          title,
           textAlign: TextAlign.center,
           style: const TextStyle(
             color: Colors.white,
